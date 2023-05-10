df27385b7c1b313ecebd9ac004cbe087af0ca375
samuel <samuelzeitler2000@gmail.com>
2023-05-10 13:03:29 +0200

    tilt frontBack and leftRight working, need to convert to azimuth and elevation and implement boundary for elevation

--- BevelGear.m
+++ BevelGear.m
@@ -1,22 +1,44 @@
-classdef BevelGear
-    %BEVELGEAR Summary of this class goes here
-    %   Detailed explanation goes here
+classdef BevelGear < handle
     
     properties
-        Property1
+        ServosObject = [];
+        composingServos = [];
     end
     
     methods
-        function obj = BevelGear(inputArg1,inputArg2)
-            %BEVELGEAR Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+        function obj = BevelGear(PORT)
+            obj.ServosObject = Servos(PORT);
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function obj = setComposingServos(obj,ID1, ID2) 
+            obj.ServosObject.checkIdAvailable(ID1);
+            obj.ServosObject.checkIdAvailable(ID2);
+            obj.composingServos = [ID1, ID2]
+        end
+
+        function torqueEnableDisable(obj,enable)
+
+            obj.ServosObject.torqueEnableDisable(obj.composingServos(1),enable);
+            obj.ServosObject.torqueEnableDisable(obj.composingServos(2),enable);
+        end
+
+        function pointStraightUp(obj)
+            %Torque must be enabled
+
+            obj.ServosObject.setAngle(obj.composingServos(1),pi);
+            obj.ServosObject.setAngle(obj.composingServos(2),pi);
+        end
+
+        function tiltRightLeft(obj, angle)
+            angle = max(min(angle,2),-2);
+            obj.ServosObject.setAngle(obj.composingServos(1),pi + angle);
+            obj.ServosObject.setAngle(obj.composingServos(2),pi - angle);
+        end
+
+        function tiltFrontBack(obj, angle)
+            angle = max(min(angle,2),-2);
+            obj.ServosObject.setAngle(obj.composingServos(1),pi + angle);
+            obj.ServosObject.setAngle(obj.composingServos(2),pi + angle);
         end
     end
 end
