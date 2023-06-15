9d3abb71facc1dccaced099f6c50947be8b11562
samuel <samuelzeitler2000@gmail.com>
2023-06-16 01:54:00 +0200

    NewBevelGear update

--- controlOfRealRobot/NewBevelGear.m
+++ controlOfRealRobot/NewBevelGear.m
@@ -1,8 +1,12 @@
 classdef NewBevelGear < handle
     
-    properties (Access = private)
+    properties (Access = public)
         ServosObject = [];
         composingServos = [];
+        angleAroundY = 0;
+        angleAroundX = 0;
+        zeroAngleFirstComposingServo = -inf;
+        zeroAngleSecondComposingServo = -inf;
     end
 
     methods (Access = public)
@@ -36,7 +40,7 @@ classdef NewBevelGear < handle
         end
 
 
-        %Rotate around the X axis (Joint 1) with Velocity
+        %Rotate around the dependent X axis (Joint 2) with Velocity
         function setVelocityAroundX(obj,velocity)
             
             successLevel = 0;
@@ -53,7 +57,7 @@ classdef NewBevelGear < handle
 
         end
 
-        %Rotate around the Y axis (Joint 2) with Velocity
+        %Rotate around the Y axis (Joint 1) with Velocity
         function setVelocityAroundY(obj,velocity)
             
             successLevel = 0;
@@ -70,7 +74,100 @@ classdef NewBevelGear < handle
 
         end
 
+        %Set the zero position for BevelGear
+        function success = setZeroPositionToCurrentPosition(obj)
+            
+            [zeroAngleFirstServo, successOne] = obj.ServosObject.getAngle(obj.composingServos(1));
+            [zeroAngleSecondServo, successTwo] = obj.ServosObject.getAngle(obj.composingServos(2));
+            
+            if successOne && successTwo
+                success = 1;
+                obj.angleAroundX = 0;
+                obj.angleAroundY = 0;
+                obj.zeroAngleFirstComposingServo  = zeroAngleFirstServo;
+                obj.zeroAngleSecondComposingServo = zeroAngleSecondServo;
+                fprintf("Successfully set Zero Position \n");
+                return
+            else
+                success = 0;
+                fprintf("Could not set Zero Position \n");
+                return
+            end
+       
+        end
 
+        %Get the rotation around Y
+        function [roty, success] = getRotationAroundY(obj)
+
+            if obj.zeroAngleFirstComposingServo == -inf || obj.zeroAngleSecondComposingServo == -inf
+                fprintf("Error getting the Rotation around X: Set the zero Position first! \n")
+                success = 0;
+                return
+            end
+
+
+            angleFirstComposingServo = obj.ServosObject.getAngle(obj.composingServos(1));
+            angleSecondComposingServo = obj.ServosObject.getAngle(obj.composingServos(2));
+
+            firstServoRotation = obj.zeroAngleFirstComposingServo - angleFirstComposingServo;
+            secondServoRotation = obj.zeroAngleSecondComposingServo - angleSecondComposingServo;
+
+            bevelFactor = 1/5;
+            roty = (-firstServoRotation + secondServoRotation) * bevelFactor;
+            
+            success = 1;
+            
+                
+        end
+
+        %Get the rotation around X
+        function [rotx, success] = getRotationAroundX(obj)
+
+            if obj.zeroAngleFirstComposingServo == -inf || obj.zeroAngleSecondComposingServo == -inf
+                fprintf("Error getting the Rotation around X: Set the zero Position first! \n")
+                success = 0;
+                return
+            end
+
+
+            angleFirstComposingServo = obj.ServosObject.getAngle(obj.composingServos(1));
+            angleSecondComposingServo = obj.ServosObject.getAngle(obj.composingServos(2));
+
+            firstServoRotation = obj.zeroAngleFirstComposingServo - angleFirstComposingServo;
+            secondServoRotation = obj.zeroAngleSecondComposingServo - angleSecondComposingServo;
+
+            bevelFactor = 1/5;
+            rotx = -(firstServoRotation + secondServoRotation) * bevelFactor;
+            
+            success = 1;
+            
+                
+        end
+
+        function [elevation, success] = getElevation(obj)
+
+            %Elevation limit should be 50° (tilted) - 90° (upright)
+
+            if obj.zeroAngleFirstComposingServo == -inf || obj.zeroAngleSecondComposingServo == -inf
+                fprintf("Error getting the Rotation around X: Set the zero Position first! \n")
+                success = 0;
+                return
+            end
+
+            angleFirstComposingServo = obj.ServosObject.getAngle(obj.composingServos(1));
+            angleSecondComposingServo = obj.ServosObject.getAngle(obj.composingServos(2));
+
+            firstServoRotation = obj.zeroAngleFirstComposingServo - angleFirstComposingServo;
+            secondServoRotation = obj.zeroAngleSecondComposingServo - angleSecondComposingServo;
+
+            bevelFactor = 1/5;
+            rotx = -(firstServoRotation + secondServoRotation) * bevelFactor;
+            roty = (-firstServoRotation + secondServoRotation) * bevelFactor;
+
+            elevation = pi/2 - acos(cos(rotx)*cos(roty));
+
+
+        end
 
      
     end
