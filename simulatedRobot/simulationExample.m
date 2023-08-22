629e8f7e8a35a3dece96dd8d3196c526b807e32f
samuel <samuelzeitler2000@gmail.com>
2023-08-22 15:59:46 +0200

    simulation stil has the bug

--- simulatedRobot/simulationExample.m
+++ simulatedRobot/simulationExample.m
@@ -5,33 +5,33 @@ clear()
 clc
 close
 
-%% Setup Frames
-frame1 = CustomFrame([0;0;0], [], 'frame1');
-frame2 = CustomFrame([0;0;100], frame1, 'frame2');
-
+%% Setup Frames and Joints of the simulated robot
+orig_frame = CustomFrame([0; 0; 0], [], 'Origin');
+joint1 = CustomJoint([0; 0; 83.51], orig_frame, 'Joint 1', 'y');
+joint2 = CustomJoint([0;0;0], joint1, 'Joint 2', 'x');
+joint3 = CustomJoint([0;0;119.35], joint2, 'Joint 3', 'z');
+joint4 = CustomJoint([0;0;163.99], joint3, 'Joint 4', 'x');
+endeffector_frame = CustomFrame([0;0;218.86], joint4, 'Endeffector');
 
 %% Setup Links of the simulated robot
-link = CustomLink(frame1, frame2, 'b');  % Blue
+link1 = CustomLink(orig_frame, joint1, 'r');  % Red
+link2 = CustomLink(joint1, joint2, 'g');  % Green
+link3 = CustomLink(joint2, joint3, 'b');  % Blue
+link4 = CustomLink(joint3, joint4, 'y');  % Yellow
+link5 = CustomLink(joint4, endeffector_frame, 'm');  % Magenta
 
 
 %% Setup simulated robot
-simulatedRobot = SimulatedRobot([], [link], [frame1, frame2]);
+simulatedRobot = SimulatedRobot([joint1, joint2, joint3, joint4], [link1, link2, link3, link4, link5], [orig_frame, endeffector_frame]);
 
-% Animate initial movement to a non-singularity position
-for step = 1:100
-   
+%% Move the robot to a non-singularity position
+for i = 1:100
+    for j = 1:4
+        simulatedRobot.joints(j).rotate(0.01)
+    end
 
-    % simulatedRobot.joints(1).setAngle(simulatedRobot.joints(1).angle + 0.01)
-    frame1.rotate(0.01,'x')
-
-    frame2.rotate(0.01,'z')
-    
-    % simulatedRobot.display(draw_frames)
-    % drawnow
-
+    simulatedRobot.display(0)
+    drawnow
 end
-
-
-simulatedRobot.display(1)
 
 
