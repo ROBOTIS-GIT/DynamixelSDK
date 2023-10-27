bd6c6f8d14b97714dd2c4e7ebafe3de212def394
Friedsam2000 <samuelzeitler2000@gmail.com>
2023-10-27 16:23:04 +0200

    multiple null space uses (issues with singularity filter)

--- simulatedRobot/actionScripts/nullspaceControl.m
+++ simulatedRobot/actionScripts/nullspaceControl.m
@@ -18,9 +18,8 @@ sr = SimulatedRobot();
 sr.setQ([0.3; 0.3; 0.5; 0.5])
 sr.draw(0)
 
-
 % Desired position and allowed deviation in [mm]
-x_desired =  [-300, -300, 300]';
+x_desired =  [0, 100, 300]';
 epsilon = 5;
 scatter3(x_desired(1), x_desired(2), x_desired(3), (epsilon^2) * pi, 'm', 'filled');
 
@@ -50,68 +49,33 @@ for timesteps = 1:max_timesteps
     % Compute pseudo inverse
     pinvJ = pinv(J);
 
-    % Filter out singularity movements
-    u = (J * J')/norm(J * J') * u;
-
-
     %% Nullspace Operator
-    % Possible additional optimization goals:
-    % 1. Penalize high joint angles (maybe quadratic, towards the max more, specific joints more)
-    % 2. Desire a specific z-Axis of the TCP
-    % 3. Penalize low shoulder gear elevations
 
     % Calculate Nullspace Operator
     N = (eye(4) - pinvJ * J);
     q = sr.getQ;
 
-    % 1. 
-    % alpha = 0.1;
-    % q_dot = pinvJ * u - alpha*N*q;
+    % Desire a specific z-Axis of the TCP
+    z_desired = [-1;0;0];
+    dHdQ_z = numericDiff(@H_z_desired, q, z_desired);
 
-    % % 2 
-    % alpha = 10;
-    % z_desired = [sin(timesteps/100); -sin(timesteps/100); cos(timesteps/100)];
-    % z_desired = z_desired/norm(z_desired);
-    % 
-    % % Small change in joint angles
-    % delta_q = 1e-6;
-    % 
-    % % Initialize dHdQ
-    % dHdQ = [0, 0, 0, 0];
-    % 
-    % % For each joint angle
-    % for i = 1:4
-    %     % Add small change to joint i
-    %     q_plus = q;
-    %     q_plus(i) = q_plus(i) + delta_q;
-    % 
-    %     % Subtract small change from joint i
-    %     q_minus = q;
-    %     q_minus(i) = q_minus(i) - delta_q;
-    % 
-    %     % Compute H(q) = 1/2 * (z(q) - z_desired) ^2
-    %     H_plus = computeH(q_plus,z_desired);
-    %     H_minus = computeH(q_minus,z_desired);
-    % 
-    %     % Compute derivative
-    %     dHdQ(i) = (H_plus - H_minus) / (2 * delta_q);
-    % end
-    % 
-    % q_dot = pinvJ * u - alpha*N*dHdQ';
+    % Secure minimum shoulder elevation
+    elevation_min = deg2rad(50);
+    dHdQ_elevation = numericDiff(@H_shoulder_elevation, q, elevation_min);
 
-    % % 3. maximize shoulder elevation --> straight line start to finish
-    alpha = 1; % use -1 to minimize elevation
-    dHdQ = [(cos(q(2))*sin(q(1)))/sqrt(1-(cos(q(2))*cos(q(1)))^2), (cos(q(1))*sin(q(2)))/sqrt(1-(cos(q(2))*cos(q(1)))^2), 0, 0];
-    q_dot = pinvJ * u - alpha*N*dHdQ';
-
-
-    % do nothing with the nullspace --> straight line start to finish
-    % q_dot = pinvJ * u;
-
+    % Combine both goals
+    dHdQ = dHdQ_z + dHdQ_elevation;
 
     %%
     % Update joint angles based on computed joint velocities
+    %
+
+
+    q_dot = pinvJ * u - N*dHdQ';
+
     sr.setQ(q + q_dot*dt)
+
+
 
     % Display the robot
     plot3(tcp_positions(1,1:timesteps), tcp_positions(2,1:timesteps), tcp_positions(3,1:timesteps), 'k');
@@ -134,16 +98,55 @@ for timesteps = 1:max_timesteps
     if toc(outerTic) < timesteps*dt
         pause(timesteps*dt-toc(outerTic))
     end
+end
+
+
+function H = H_z_desired(q, z_desired)
+    z_desired = z_desired/norm(z_desired);
+    g_A_tcp = SimulatedRobot.roty(q(1)) * SimulatedRobot.rotx(q(2)) * SimulatedRobot.rotz(q(3)) * SimulatedRobot.rotx(q(4));
+    z_q = g_A_tcp * [0;0;1];
+    z_q = z_q/norm(z_q);
+    H = 1/2 * norm(z_q-z_desired)^2; % <= 1;
+
+    % fprintf("H z = %.2f\n", H)
+
+end
+
+function H = H_shoulder_elevation(q, elevation_min)
+    elevation = SimulatedRobot.getShoulderElevation(q);
+    distance_to_min_elevation = elevation-elevation_min;
+    a = 0.01;
+    b = 100;
+    H = a*exp(-b*distance_to_min_elevation); % H <= 0.01 for elevation > min_elevation, H --> inf for elevation < min_elevation
+    
+    % fprintf("H elevation = %.2f\n", H)
     
 end
 
-% Compute H(q) = 1/2 * (z(q) - z_desired) ^2
-function H = computeH(q,z_desired)
-    
-    g_A_tcp = SimulatedRobot.roty(q(1)) * SimulatedRobot.rotx(q(2)) * SimulatedRobot.rotz(q(3)) * SimulatedRobot.rotx(q(4));
-    z_q = g_A_tcp * [0;0;1];
-    H = 1/2 * ((z_q(1)-z_desired(1))^2 + (z_q(2)-z_desired(2))^2 + (z_q(3)-z_desired(3))^2);
-    
+function dHdQ = numericDiff(H, q, varargin)
+    % Define a small change in joint angles
+    delta_q = 1e-6;
+
+    % Initialize dHdQ
+    dHdQ = zeros(1,4); % Column vector initialization
+
+    % For each joint angle
+    for i = 1:4
+        % Create a copy of q for perturbation
+        q_plus = q;
+        q_minus = q;
+        
+        % Add and subtract small change to joint i
+        q_plus(i) = q_plus(i) + delta_q;
+        q_minus(i) = q_minus(i) - delta_q;
+
+        % Compute H(q)
+        H_plus = H(q_plus, varargin{:});
+        H_minus = H(q_minus, varargin{:});
+
+        % Compute derivative
+        dHdQ(1,i) = (H_plus - H_minus) / (2 * delta_q);
+    end
 end
 
 
