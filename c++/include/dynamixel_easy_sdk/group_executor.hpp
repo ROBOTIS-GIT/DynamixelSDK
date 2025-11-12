6d55bc9c58d242fd9ea10200ae84dc15fa30f64f
Hyungyu Kim <kimhg@robotis.com>
2025-11-12 11:17:41 +0900

    remove include control_table sentence in groupExecutor
    Signed-off-by: Hyungyu Kim <kimhg@robotis.com>

--- c++/include/dynamixel_easy_sdk/group_executor.hpp
+++ c++/include/dynamixel_easy_sdk/group_executor.hpp
@@ -25,7 +25,6 @@
 #include <vector>
 
 #include "dynamixel_sdk/dynamixel_sdk.h"
-#include "dynamixel_easy_sdk/control_table.hpp"
 #include "dynamixel_easy_sdk/dynamixel_error.hpp"
 #include "dynamixel_easy_sdk/data_types.hpp"
 
